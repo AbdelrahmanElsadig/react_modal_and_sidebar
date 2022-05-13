589299008fe6d1fff6f1a8514f2ecd24cb463a1e
Abdul Elsadig <abdelrahmanelsadig1@gmail.com>
2022-05-13 14:56:39 +0200

    fixed modal overlay bug

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,7 +5,7 @@ import { providerProps } from '../types/types';
 const Modal = () => {
     const { isModalOpen, isSidebarOpen, dispatch }: providerProps = useContext<any>(AppContext);
     const style = isModalOpen ? 'scale-100' : 'scale-0';
-    const style2 = isModalOpen ? '' : 'opacity-0';
+    const style2 = isModalOpen ? 'opacity-50' : 'opacity-0';
     return (
         <>
             <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
@@ -15,7 +15,7 @@ const Modal = () => {
             </div>
 
             <div>
-                <div className={`fixed ${style2} pointer-events-none transition-all ease-in-out duration-300 left-0 top-0 right-0 bottom-0 bg-black opacity-50`}></div>
+                <div className={`fixed ${style2} pointer-events-none transition-all ease-in-out duration-300 left-0 top-0 right-0 bottom-0 bg-black`}></div>
                 <div className={`fixed ${style} transition-all ease-in-out duration-300 flex justify-center items-center h-44 bg-neutral-200 z-40 p-8 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] rounded-md`}>
                     <p className="text-2xl font-bold">Modal Content Here...</p>
                     <FaTimes onClick={() => dispatch({ type: 'CLOSE MODAL' })} className='w-6 h-6 absolute top-2 right-2 cursor-pointer fill-gray-600 duration-200 ease-in-out hover:fill-red-600' />
